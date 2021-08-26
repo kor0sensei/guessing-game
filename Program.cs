042fb86b38a520469b8c0ad9b17edeeb896a7697
kor0sensei <averyolmstead@comcast.net>
2021-08-26 11:19:22 -0500

    added difficulty with different amounts of guesses for each difficulty and user select difficulty

--- Program.cs
+++ Program.cs
@@ -8,11 +8,40 @@ namespace GuessingGame
             int secretNumber = new Random().Next(1, 100);
 
             int GuessCount = 0;
+            int DifficultyLevel = 0;
 
             Console.WriteLine("Guess the Secret Number Bru");
+            Console.WriteLine("Please enter a difficulty level between 1 and 3:");
+            while(DifficultyLevel == 0)
+            {
+                switch(int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        DifficultyLevel = 1;
+                        GuessCount = 8;
+                        Console.WriteLine("Difficulty set to 'easy.' You have eight guesses to find the secret number.");
+                        break;
+
+                    case 2:
+                        DifficultyLevel = 2;
+                        GuessCount = 6;
+                        Console.WriteLine("Difficulty set to 'medium.' You have six guesses to find the secret number.");
+                        break;
+
+                    case 3:
+                        DifficultyLevel = 3;
+                        GuessCount = 4;
+                        Console.WriteLine("Difficulty set to 'hard.' You have four guesses to find the secret number.");
+                        break;
+
+                    default:
+                        Console.WriteLine("You must enter a number between 1 and 3 to continue:");
+                        break;
+                }
+            }
             Console.WriteLine("Type a number between 1 and 100 and hit enter Bru");
             int guess = int.Parse(Console.ReadLine());
-            while (GuessCount < 3)
+            while (GuessCount > 1)
             {
                 if (guess == secretNumber) 
                 {
@@ -23,7 +52,7 @@ namespace GuessingGame
                 {
                     string HighLowString = "too high";
                     string GuessString = "guesses";
-                    if ((4-GuessCount) == 1)
+                    if ((GuessCount) == 2)
                     {
                         GuessString = "guess";
                     }
@@ -31,8 +60,8 @@ namespace GuessingGame
                     {
                         HighLowString = "too low";
                     }
-                    GuessCount++;
-                    Console.WriteLine($"Nah Bru {guess} is {HighLowString} Bru, You have {4-GuessCount} {GuessString} left Bru, Try again Bru");
+                    GuessCount--;
+                    Console.WriteLine($"Nah Bru {guess} is {HighLowString} Bru, You have {GuessCount} {GuessString} left Bru, Try again Bru");
                     guess = int.Parse(Console.ReadLine());
                 }
             }
